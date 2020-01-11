6db1da28f083de28acdceb42c1407f0685b939cd
Ross Johnstone <rhjohnstone@gmail.com>
2020-01-11 23:23:44 +0900

    Play tune, only keep used note files

---
+++ ode/play.py
@@ -0,0 +1,22 @@
+from pygame import mixer
+from time import sleep
+
+mixer.init()
+c = mixer.Sound("c.wav")
+d = mixer.Sound("d.wav")
+e = mixer.Sound("e.wav")
+f = mixer.Sound("f.wav")
+g = mixer.Sound("g.wav")
+
+first_bit = [e, e, f, g, g, f, e, d, c, c, d, e]
+
+line1 = first_bit + [e, d, d]
+line2 = first_bit + [d, c, c]
+line3 = [d, d, e, c, d, e, f, e, c, d, e, f, e, d, c, d, g]
+line4 = line2
+
+sleep(2)
+for note in line1+line2+line3+line4:
+    note.play()
+    sleep(0.4)
+    note.stop()
