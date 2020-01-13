a98c67ebaf9b9f63d3b603d4e1a0c8464773bb9f
Ross Johnstone <rhjohnstone+gitlab@pm.me>
2020-01-13 17:10:49 +0900

    Fix loading output

--- office-thermometer/plot.py
+++ office-thermometer/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-x, ten_y = np.loadtxt("output.txt")
+x, ten_y = np.loadtxt("output.txt").T
 
 plt.plot(x, ten_y/10)
 plt.show()
