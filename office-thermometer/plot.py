6151f6855e159cfc8eafc6a18dc36efa05909ece
Ross Johnstone <rhjohnstone@gmail.com>
2020-01-22 21:16:09 +0900

    LED strength varies. Plot graph

--- office-thermometer/plot.py
+++ office-thermometer/plot.py
@@ -1,8 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
+from datetime import datetime
 
-x, ten_y = np.loadtxt("output.txt").T
+x, ten_y = np.loadtxt("output.txt", dtype=int).T
+time = np.array([datetime.fromtimestamp(t) for t in x])
 
-plt.plot(x, ten_y/10)
+h = 5
+fs = 14
+fig, ax = plt.subplots(1, 1, figsize=(2*h,h))
+ax.plot(time, ten_y/10)
+ax.set_ylabel('Temperature (C)', fontsize=fs)
+ax.set_xlabel('Time', fontsize=fs)
+ax.set_title('"Winter" 2020 in the Tokyo Office', fontsize=fs)
+fig.tight_layout()
+fig.savefig('office-temp.png')
 plt.show()
--- office-thermometer/run.py
+++ office-thermometer/run.py
@@ -1,56 +1,53 @@
-from gpiozero import LED
+from gpiozero import PWMLED
 from w1thermsensor import W1ThermSensor
 import time
-import numpy as np
-
-
-def update(final=False):
-    global data
-    if final:
-        data = data[data[:,0]>0]
-    with open(output_file, "a") as outf:
-        np.savetxt(outf, data)
-    if not final:
-        data = np.zeros((buffer, 2), dtype=int)
 
 
 def intround(x):
     return int(round(x))
 
 
-red = LED(18)
-blue = LED(24)
+red = PWMLED(18)
+blue = PWMLED(24)
 thermometer = W1ThermSensor()
 
+lowest = 24
+low = 25.5
+high = 26.5
+highest = 28
+
+cold = lambda temp: 1 - (temp - lowest) / (low - lowest)
+hot = lambda temp: (temp - high) / (highest - high)
+
+wait = 10
+
 output_file = "output.txt"
-with open(output_file, "w") as outf:
-    pass
 
-upper = 27
-lower = 25.5
-buffer = 10
-
-data = np.zeros((buffer, 2), dtype=int)
-row = 0
+latest = time.time()
 try:
-    while True:
-        current_time = time.time()
-        temp = thermometer.get_temperature(W1ThermSensor.DEGREES_C)
-        if temp > upper:
-            red.on()
-            blue.off()
-        elif temp < lower:
-            blue.on()
-            red.off()
-        else:
-            red.off()
-            blue.off()
-        data[row] = [intround(current_time), intround(10*temp)]
-        row = (row + 1) % buffer
-        if row == 0:
-            update()
-        time.sleep(1)
+    with open(output_file, "w") as outf:
+        while True:
+            current_time = time.time()
+            temp = thermometer.get_temperature(W1ThermSensor.DEGREES_C)
+            '''print(temp)'''
+            if temp <= lowest:
+                red.value = 0
+                blue.value = 1
+            elif lowest < temp <= low:
+                red.value = 0
+                blue.value = cold(temp)
+            elif low < temp < high:
+                red.value = 0
+                blue.value = 0
+            elif high <= temp < highest:
+                blue.value = 0
+                red.value = hot(temp)
+            elif highest <= temp:
+                blue.value = 0
+                red.value = 1
+            if current_time - latest >= 60:
+                outf.write(f"{intround(current_time)} {intround(10*temp)}\n")
+                latest = current_time
+            time.sleep(1)
 except KeyboardInterrupt:
-    if row > 0:
-        update(True)
-
+    pass
